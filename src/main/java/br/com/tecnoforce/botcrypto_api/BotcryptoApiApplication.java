614ac028d2b9ef0c9db3a086638701334f09bb59
Ricardo Cavalcanti <ricardocavalcanti@outlook.com>
2025-04-20 08:33:03 -0300

    chore: criar projeto Spring Boot

---
+++ src/main/java/br/com/tecnoforce/botcrypto_api/BotcryptoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tecnoforce.botcrypto_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotcryptoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotcryptoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tecnoforce/botcrypto_api/BotcryptoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecnoforce.botcrypto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotcryptoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
